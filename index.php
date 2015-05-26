5231d7f3e514560df39c1b1b89a39f28902a200b
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 16:27:41 +0200

    message

--- index.php
+++ index.php
@@ -16,25 +16,5 @@
 </head>
 <body>
 	<h1>{{1+1}}</h1>
- 	<header>Bienvenue sur le PolyCoin</header>
- 	<section ng-controller="menuController">
- 		<ul class="nav nav-pills">
-
- 			<li ng-class="menuController.estChoisi(1)">
- 				<a>Accueil</a>
- 			</li>
- 			
- 			<li ng-class="menuController.estChoisi(2)">
- 				<a href ng-click="ong=2">Articles</a>
- 			</li>
- 			
- 		</ul>
- 		 	
-
- 		<div article class="panel" ng-show="estChoisi(2)"></div>
- 	</section>
-	<script type ="text/javascript" src="polycoin.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
 </body>
 </html>
