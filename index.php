37890fbf83f0819496f101bf853321c0165ac94b
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 16:33:46 +0200

    message

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
  	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
 </head>
 <body>
-	<h1>{{1+1}}</h1>
+	<div>
+		<h1>{{1+1}}</h1>
+	</div>
 </body>
 </html>
