f243825790357ceb9460db83c72241b7d2ad7c91
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 12:17:47 +0200

    message

--- index.php
+++ index.php
@@ -49,6 +49,6 @@
 		</section>
 
 	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
-	<script type = "text/javascript" src="./app.js"></script>
+	<script type = "text/javascript" src="./polycoin.js"></script>
 	</body>
 </html>
