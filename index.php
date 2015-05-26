bccd261d3b6bb651ed5aa1f43cab8ec26847648a
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 16:31:00 +0200

    message

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="fr" ng-app="polycoin">
 <head>
-  	<meta charset="utf-8">
  	<title>Accueil</title>
  	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
 </head>
