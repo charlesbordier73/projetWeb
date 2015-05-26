0603a002577f5588c78453414325f58b0852967b
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 16:29:56 +0200

    message

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <!doctype html>
 <html lang="fr" ng-app="polycoin">
 <head>
-  <meta charset="utf-8">
-  <title>Accueil</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-
+  	<meta charset="utf-8">
+ 	<title>Accueil</title>
+ 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
 </head>
 <body>
 	<h1>{{1+1}}</h1>
