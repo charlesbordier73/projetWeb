e81cda43e4e7de952dea3b2eabd4ca4b68c550ab
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 16:25:19 +0200

    message

--- index.php
+++ index.php
@@ -1,14 +1,40 @@
 <!doctype html>
-<html ng-app>
-  <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-  </head>
-  <body>
-    <div>
-      <label>Name:</label>
-      <input type="text" ng-model="yourName" placeholder="Enter a name here">
-      <hr>
-      <h1>Hello {{1+1}}!</h1>
-    </div>
-  </body>
+<html lang="fr" ng-app="polycoin">
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+
+</head>
+<body>
+	<h1>{{1+1}}</h1>
+ 	<header>Bienvenue sur le PolyCoin</header>
+ 	<section ng-controller="menuController">
+ 		<ul class="nav nav-pills">
+
+ 			<li ng-class="menuController.estChoisi(1)">
+ 				<a>Accueil</a>
+ 			</li>
+ 			
+ 			<li ng-class="menuController.estChoisi(2)">
+ 				<a href ng-click="ong=2">Articles</a>
+ 			</li>
+ 			
+ 		</ul>
+ 		 	
+
+ 		<div article class="panel" ng-show="estChoisi(2)"></div>
+ 	</section>
+	<script type ="text/javascript" src="polycoin.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+</body>
 </html>
