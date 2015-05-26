479e9027f3e5acb9c755a663524d507cf086f4d2
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-26 17:00:55 +0200

    message

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.js"></script>
 
 </head>
 <body>
