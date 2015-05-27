92e572b9cec92e7ada9bea58c5f82d37ed8c0c15
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-27 22:25:09 +0200

    message

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ include 'header.php';
     	$idclient = $_COOKIE['id'];
     	echo $idclient;
     	$token = mysql_query("SELECT token FROM Vendeur WHERE id = '$idclient'");
+    	echo $token;
     	$loginTab = mysql_query("SELECT login FROM Vendeur WHERE id = '$idclient'");
     	$login=$loginTab["login"];
     	echo $loginTab;
