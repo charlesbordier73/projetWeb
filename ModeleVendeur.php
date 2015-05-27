48de79824ad9d1492e237a0fc212b6ac89cfa693
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-27 18:58:29 +0200

    message

--- ModeleVendeur.php
+++ ModeleVendeur.php
@@ -23,7 +23,7 @@ error_reporting(E_ALL);
         $login = $_POST['login'];
         echo $login;
         $mdp = mysql_query("SELECT mdp from Vendeur where login='$login'");
-        echo $mdp;
+        echo "<br>" $mdp"<br>";
             if($_POST['mdp']==$mdp){
 
                 $id=mysql_query("SELECT id from Vendeur where login='$login'");
