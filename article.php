31eddf330c5e9f8bf1498646d2b3afcd3ae151f2
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-27 05:15:02 +0200

    message

--- article.php
+++ article.php
@@ -1,5 +1,34 @@
-coucou
-<!--
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Polycoin</title>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+ 	<header>Bienvenue sur le PolyCoin</header>
+ 		<ul class="nav nav-pills">
+
+ 			<li>
+ 				<a href="index.php">Accueil</a>
+ 			</li>
+ 			
+ 			<li>
+ 				<a href="article.php">Articles</a>
+ 			</li>
+ 			
+ 		</ul>
+ 		 
+
 <table border="1">
 	<tr>
 		<th>Nom</th>
@@ -34,4 +63,7 @@ coucou
 	$connexion=null;
 ?>
 </table>
--->
+
+
+</body>
+</html>
