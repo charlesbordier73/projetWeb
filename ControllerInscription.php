03b6672906eeef05c4f9652f96ed6b17baeda99f
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-27 10:19:05 +0200

    message

--- ControllerInscription.php
+++ ControllerInscription.php
@@ -1,3 +1,35 @@
-<?php
-echo 1;
+<?php 
+echo "1";
+//récupération des champs
+if(isset($_POST['nom']))
+    $nom=$_POST['nom'];
+else
+    $nom="";
+
+if(isset($_POST['prenom']))
+    $prenom=$_POST['prenom'];
+else
+    $prenom="";
+
+if(isset($_POST['email']))
+    $email=$_POST['email'];
+else
+    $email="";
+
+if(isset($_POST['tel']))
+    $tel=$_POST['tel'];
+else
+    $tel="";
+
+if(isset($_POST['login']))
+    $login=$_POST['login'];
+else
+    $login="";
+
+if(isset($_POST['mdp']))
+    $mdp=$_POST['mdp'];
+else
+    $mdp="";
+
+echo 2;
 ?>
