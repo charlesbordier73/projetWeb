297ae125d4e008b1f445b599488d2d7a301e5c29
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-28 15:06:28 +0200

    message

--- ModeleArticle.php
+++ ModeleArticle.php
@@ -15,6 +15,19 @@ error_reporting(E_ALL);
         $sql = "SELECT idVendeur, nomArticle, description from Article where nomArticle LIKE '%$name%'";
          // on insère les informations du formulaire dans la table 
         mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+
+        while($article = mysql_fetch_array($sql))
+    {
+?>
+    <table>
+    <tr>
+        <td><?php echo $article['nomArticle']; ?></td>
+        <td><?php echo $article['description']; ?></td>
+        <td><?php echo $article['DateMiseEnLigne']; ?></td>
+    </tr>
+    </table>
+<?php }
+    $resultats->closeCursor();
     }
     
     mysql_close();  // on ferme la connexion 
