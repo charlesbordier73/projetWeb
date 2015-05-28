d86a89291c56ad6185d2d56b3f14f2321a199d3a
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-28 10:54:44 +0200

    message

--- deconnexion.php
+++ deconnexion.php
@@ -1,9 +1,9 @@
 <?php
-	
-	setcookie("id", "", time()-3600);
-	unset($_COOKIE['token']);
-    //header('Location:index.php');
-	echo "toto";
-	echo $_COOKIE['id'];
+	if(isset($_COOKIE['id']) && isset($_COOKIE['token']))
+    {	
+    	setcookie("id", "", time()-1,"/");
+		setcookie("token", "", time()-1,"/");
+    	header('Location:index.php');
+    }
 ?>
 
