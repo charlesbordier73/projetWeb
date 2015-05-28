9a662398d274b3da0bb03ebe63fcffe92436a3b2
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-28 19:09:11 +0200

    message

--- ModeleArticle.php
+++ ModeleArticle.php
@@ -17,7 +17,7 @@ include 'header.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
+</head>
 <table border ="1">
     <tr>
         <th>Nom</th>
