6e4b828cb4445bc8a4c8fa9d8f9bdc46867f8dca
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-29 15:01:54 +0200

    message

--- ModeleArticle.php
+++ ModeleArticle.php
@@ -37,6 +37,7 @@ error_reporting(E_ALL);
     }
 
     else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
+        echo "totorqt";
         $sql = "SELECT idVendeur, nomArticle, description, DateMiseEnLigne from Article where idVendeur= '$id'";
         $resultats = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
 
