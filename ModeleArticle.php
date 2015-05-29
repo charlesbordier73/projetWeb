94ebeaca8d9c847aa48facf4e30f5cfdfc711342
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-29 16:05:52 +0200

    message

--- ModeleArticle.php
+++ ModeleArticle.php
@@ -31,13 +31,13 @@ error_reporting(E_ALL);
     mysql_select_db($bdd,$link) or die(mysql_error()); 
      
     if(isset($name) && !empty($name)){
+        echo "toto";
         $sql = "SELECT idVendeur, nomArticle, description, DateMiseEnLigne from Article where nomArticle LIKE '%$name%'";
         $result = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
     }
 
 
     else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
-        echo 'toto';
         $sql = "SELECT idVendeur, nomArticle, description, DateMiseEnLigne from Article where idVendeur= '$id'";
         $resultats = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
         print_r($resultats);
