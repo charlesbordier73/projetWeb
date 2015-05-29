d0db3cbb5a04c38105cc0e210ece43672aa92e1f
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-29 17:39:46 +0200

    message

--- ControllerArticle.php
+++ ControllerArticle.php
@@ -6,15 +6,15 @@ if(isset($_GET['name']))
 else
     $name="";
 
-if(!empty($name))      
-    { 
-        echo "toto";
+if(!empty($name)) 
+{ 
+
         include 'ModeleArticle.php';
         include 'catalogue.php';
 
-    }
-
+} 
 else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
+    echo "toto";
     include 'ModeleArticle.php';
     include 'mesArticles.php'; 
  }
