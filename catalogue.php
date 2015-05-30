5b77a722d952de4791a10dbcafe950ed903abe85
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 01:27:43 +0200

    message

--- catalogue.php
+++ catalogue.php
@@ -40,13 +40,11 @@ if(isset($_GET['name'])){
 				<td>
 				<?php
 				$img=mysql_fetch_array($images);
-				while($img = mysql_fetch_array($images)){
-					if($img['idArticle']==$article['idArticle']){
-						?>
-							<img src="<?php echo $img[1];?>" width="200" height="150" alt="" />
-						<?php
-							
-					}
+				if($img['idArticle']==$article['idArticle']){
+					?>
+						<img src="<?php echo $img[1];?>" width="200" height="150" alt="" />
+					<?php
+						
 				}
 				?>
 				</td>
