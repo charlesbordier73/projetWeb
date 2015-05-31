7c3b295694cf16ad2898fe58cd44dbd3a699e864
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 19:09:50 +0200

    message

--- ModeleArticle.php
+++ ModeleArticle.php
@@ -18,9 +18,7 @@ error_reporting(E_ALL);
         }
         else if(isset($_POST['validationAjout'])){
             $lien= "./images/" . $lienMid;
-            echo $lien;
-            $insertSql = "INSERT INTO Article(idVendeur, nomArticle, description, DateMiseEnLigne, url) VALUES ('$idV','$nomAj','$descrAj','$dateAj', '$lien'";
-            
+            $insertSql = "INSERT INTO Article(idVendeur, nomArticle, description, DateMiseEnLigne, url) VALUES ('$idV','$nomAj','$descrAj','$dateAj', '$lien')";
             $query=mysql_query($insertSql) or die('Erreur SQL !'.$insertSql.'<br>'.mysql_error());
             if($query==1){
                 $upl='./images/';
