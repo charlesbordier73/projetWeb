794e914b50a6a75d8a5ef2d245602531a88ece66
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 23:56:11 +0200

    message

--- catalogue.php
+++ catalogue.php
@@ -43,8 +43,8 @@ if(isset($_GET['name'])){
 			    <td>
 			    	<form method="POST" action="ControllerArticle.php">
 			    		<input type="submit" value="Détails" name="detailArt">
-			    		<?php echo "toto"; echo $articles['idArticle'];?>
-			    		<input type="hidden" value="<?php echo $articles['idArticle'];?>" name="idArt">
+			    		<?php echo "toto"; echo $article['idArticle'];?>
+			    		<input type="hidden" value="<?php echo $article['idArticle'];?>" name="idArt">
 			    	</form>
 			    </td>
 
