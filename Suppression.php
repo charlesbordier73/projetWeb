987179dc1eabcaed7a2f7da966c758ee31ff8ce8
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 19:58:49 +0200

    message

--- Suppression.php
+++ Suppression.php
@@ -15,7 +15,7 @@ error_reporting(E_ALL);
         $sql = "DELETE from Article where idArticle= '$idArticle'";
         //$img = "SELECT url from Article where idArticle= '$idArticle'";
         //mysql_query($img)) or die('Erreur SQL !'.$img.'<br>'.mysql_error());
-        mysql_query($sql)) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
+        $res=mysql_query($sql)) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
         /*if($sql==1){
             unlink(realpath($img));
         };*/
