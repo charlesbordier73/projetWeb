b86fa3099674eb1d95af136c4d9c8dd2466fb865
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 14:53:44 +0200

    message

--- ControllerArticle.php
+++ ControllerArticle.php
@@ -18,12 +18,12 @@ else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
         $idArticle=$_POST['id'];
         include 'Suppression.php';
         include 'mesArticles.php';
-    /*}   
+    }   
     else if(isset($_POST['modifier'])){
         $idArticle=$_POST['idmod'];
         $nomArticle=mysql_escape_string($_POST['nom']);
         $descrArticle=mysql_escape_string($_POST['descr']);
-        include 'Modification.php';*/
+        include 'Modification.php';
     }
     else if(isset($_POST['validationModif'])){
         $idArticleMod=$_POST['idModif'];
@@ -38,8 +38,8 @@ else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
     else if(isset($_POST['validationAjout'])){
         $nomAj=mysql_escape_string($_POST['nomAj']);
         $descrAj=mysql_escape_string($_POST['descrAj']);
-        //$dateAj=$_POST['date']);
-        //$idV=$_POST['idAjout']);
+        $dateAj=$_POST['date']);
+        $idV=$_POST['idAjout']);
         $lien = $_FILES['lien'];
         //include 'ModeleArticle.php';
         //include 'mesArticles.php'; 
