95aaaf8f0d04290b64c3ae2cc44c491b284d9d52
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 15:39:59 +0200

    message

--- ControllerArticle.php
+++ ControllerArticle.php
@@ -40,7 +40,7 @@ else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
         $descrAj=mysql_escape_string($_POST['descrAj']);
         $dateAj=$_POST['dateAj'];
         $idV=$_POST['idAj'];
-        $lien = $_FILES['lien'];
+        $lien = trim($_FILES['lien']);
         include 'ModeleArticle.php';
         include 'mesArticles.php'; 
 
