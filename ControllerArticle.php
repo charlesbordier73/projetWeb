2b79d4e6ed9e60caf4ecbfb2386cd05d850374a2
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 19:52:34 +0200

    message

--- ControllerArticle.php
+++ ControllerArticle.php
@@ -16,6 +16,7 @@ if(!empty($name))
 else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
     if(isset($_POST['supprimer'])){
         $idArticle=$_POST['id'];
+        echo "toto";
         include 'Suppression.php';
         include 'mesArticles.php';
     }   
