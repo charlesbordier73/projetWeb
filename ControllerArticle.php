3275a8754743a84762b0e2e2dc90d0612aaa3a75
Charles Bordier <charles.bordier@yahoo.fr>
2015-05-31 18:54:15 +0200

    message

--- ControllerArticle.php
+++ ControllerArticle.php
@@ -40,11 +40,10 @@ else if(isset($_COOKIE['id']) && isset($_COOKIE['token'])){
         $descrAj=mysql_escape_string($_POST['descrAj']);
         $dateAj=$_POST['dateAj'];
         $idV=$_POST['idAj'];
-    print_r($_FILES);
         $lienMid = trim($_FILES['lien']['name']);
         echo "totot";
         echo $lienMid;
-        //include 'ModeleArticle.php';
+        include 'ModeleArticle.php';
         include 'mesArticles.php'; 
 
     }
--- ModeleArticle.php
+++ ModeleArticle.php
@@ -17,14 +17,14 @@ error_reporting(E_ALL);
             mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
         }
         else if(isset($_POST['validationAjout'])){
-            $lien= "./images" . $lienMid['name'];
-            echo $lien['name'];
+            $lien= "./images" . $lienMid;
+            echo $lien;
             $insertSql = "INSERT INTO `Article`(`idVendeur`, `nomArticle`, `description`, `DateMiseEnLigne`, `url`) VALUES ('$idV','$nomAj','$descrAj','$dateAj', '$lien'";
             
             $query=mysql_query($insertSql) or die('Erreur SQL !'.$insertSql.'<br>'.mysql_error());
             if($query==1){
                 $upl='./images/';
-                move_uploaded_file($lien['name'], $upl.$lien['name']);
+                move_uploaded_file($lien, $upl.$lien);
             }
         }
         else{
